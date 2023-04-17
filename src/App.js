31c8762c10e137c8baf4253f00b5b299ed5ea9da
Shashank-2021 <workingwithshashank@gmail.com>
2023-04-17 14:23:32 +0530

    Sets up main components flow

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
-
+import Head from "./components/Head";
+import Body from "./components/Body";
 function App() {
   return (
     <div>
-      <h1 className="font-bold text-3xl">My Youtube</h1>
+      <Head />
+      <Body />
     </div>
   );
 }
