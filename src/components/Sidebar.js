54f52de9ee37bd20204d992dfcbbdb2e3dc1eb8e
Shashank-2021 <workingwithshashank@gmail.com>
2023-04-17 16:55:27 +0530

    Makes the hamburger icon in header work to toggle. -Clicking on icon dispatches an action which then call a reducer fn. - Reducer fn modifies slice in our store. -Sidebar component is subscribed to  isMenuOpen state inside app slice.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,6 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  //Early Return
+  if (!isMenuOpen) return null;
+
   return (
     <div className="p-5 w-48 shadow-lg">
       <ul>
@@ -26,5 +32,4 @@ const Sidebar = () => {
     </div>
   );
 };
-
 export default Sidebar;
