edc2d85b5b3565381836c1ed0a8d4a4a521ab764
Shashank-2021 <workingwithshashank@gmail.com>
2023-04-18 20:09:28 +0530

    Renders 50 most popular indian yt videos on UI

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 const GOOGLE_API_KEY = "AIzaSyCZGcdOG3OKnSaQW_yYiQ2Hxr9EmBrL5EQ";
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_API_KEY;
